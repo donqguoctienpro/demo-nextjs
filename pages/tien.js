8ed41fcef5d099b5ffcbd912e4e8425fd78f0586
AnhTienDepTrai <dongquoctienpro@gmail.com>
2021-08-26 21:58:14 +0700

    first commit

---
+++ pages/tien.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+tien.propTypes = {
+
+};
+
+function tien(props) {
+    return (
+        <div>
+            Hello NextJs
+        </div>
+    );
+}
+
+export default tien;
